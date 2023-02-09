538680f3c7f63e35ef4f42b34d37c4c3ade4b34e
jitender4todomist <jitender.jattu@todomist.in>
2023-02-09 14:49:04 +0530

    add user profile

--- src/components/Dashboard/ShowMenu.js
+++ src/components/Dashboard/ShowMenu.js
@@ -3,26 +3,55 @@ import URI from "../../common/config";
 
 export const ShowMenu = () => {
   const [data, setData] = useState([]);
-
+  const [name, setName] = useState([]);
+  const [value, getValue] = useState("hikaru");
+  function selectName(e) {
+    getValue(e.target.value);
+  }
+  console.log(value);
+  const getName = async () => {
+    const response = await fetch(`https://api.chess.com/pub/titled/GM`);
+    const json = await response.json();
+    setName(json.players);
+  };
   const getData = async () => {
-    const response = await fetch(URI);
+    const response = await fetch(`https://api.chess.com/pub/player/${value}`);
     const json = await response.json();
     setData(json);
   };
   useEffect(() => {
+    getName();
+  }, []);
+  useEffect(() => {
     getData();
   }, []);
   return (
-    <div className="container">
-      <div className="card">
-        <img src={data.avatar} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">{data.title}</h5>
-          <h5 className="card-title">{data.name}</h5>
-          <h5 className="card-title">Username: {data.username}</h5>
-          <h5 className="card-title">followers: {data.followers}</h5>
+    <>
+      <select
+        style={{ margin: "2rem 0 0 6rem" }}
+        value={value}
+        onChange={(e) => selectName(e)}
+      >
+        <option>Select Username</option>
+        {name?.map((val, index) => {
+          return (
+            <option key={index} value={val}>
+              {val}
+            </option>
+          );
+        })}
+      </select>
+      <div className="container">
+        <div className="card">
+          <img src={data.avatar} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{data.title}</h5>
+            <h5 className="card-title">{data.name}</h5>
+            <h5 className="card-title">Username: {data.username}</h5>
+            <h5 className="card-title">followers: {data.followers}</h5>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
